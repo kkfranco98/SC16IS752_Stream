9ff68d764610f414496616c39e76b0b44dabeb63
Francesco Simonetti <kkfranco98@gmail.com>
2026-03-27 18:54:35 +0100

    format

--- SC16IS752.cpp
+++ SC16IS752.cpp
@@ -19,36 +19,40 @@
    Please keep the above information when you use this code in your project.
  */
 
-
 // #define SC16IS750_DEBUG_PRINT
 #include <SC16IS752.h>
 #include <SPI.h>
 #include <Wire.h>
 
 #ifdef __AVR__
- # define WIRE Wire
+#define WIRE Wire
 #elif defined(ESP8266) || defined(ESP32) // ESP8266/ESP32
- # define WIRE Wire
+#define WIRE Wire
 #elif ESP32 // ESP8266
- # define WIRE Wire
+#define WIRE Wire
 #else // Arduino Due
- # define WIRE Wire1
+#define WIRE Wire1
 #endif // ifdef __AVR__
-
 
 SC16IS752::SC16IS752(uint8_t prtcl, uint8_t addr_sspin) : initialized(false)
 {
   protocol = prtcl;
 
-  if (protocol == SC16IS750_PROTOCOL_I2C) {
+  if (protocol == SC16IS750_PROTOCOL_I2C)
+  {
     // Datasheet uses extra read/write bit to describe I2C address.
     // Actual address in communication has one bit shifted.
-    if ((addr_sspin >= 0x48) && (addr_sspin <= 0x57)) {
+    if ((addr_sspin >= 0x48) && (addr_sspin <= 0x57))
+    {
       device_address_sspin = addr_sspin;
-    } else {
+    }
+    else
+    {
       device_address_sspin = (addr_sspin >> 1);
     }
-  } else {
+  }
+  else
+  {
     device_address_sspin = addr_sspin;
   }
   peek_flag[SC16IS752_CHANNEL_A] = 0;
@@ -66,7 +70,8 @@ void SC16IS752::begin(uint32_t baud_A, uint32_t baud_B)
 
 void SC16IS752::beginA(uint32_t baud_A)
 {
-  if (!initialized) {
+  if (!initialized)
+  {
     Initialize();
   }
   FIFOEnable(SC16IS752_CHANNEL_A, 1);
@@ -76,7 +81,8 @@ void SC16IS752::beginA(uint32_t baud_A)
 
 void SC16IS752::beginB(uint32_t baud_B)
 {
-  if (!initialized) {
+  if (!initialized)
+  {
     Initialize();
   }
   FIFOEnable(SC16IS752_CHANNEL_B, 1);
@@ -91,7 +97,8 @@ int SC16IS752::available(uint8_t channel)
 
 int SC16IS752::read(uint8_t channel)
 {
-  if (peek_flag[channel] == 0) {
+  if (peek_flag[channel] == 0)
+  {
     return ReadByte(channel);
   }
   peek_flag[channel] = 0;
@@ -123,13 +130,16 @@ uint8_t SC16IS752::ReadRegister(uint8_t channel, uint8_t reg_addr)
 {
   uint8_t result = 0;
 
-  if (protocol == SC16IS750_PROTOCOL_I2C) { // register read operation via I2C
+  if (protocol == SC16IS750_PROTOCOL_I2C)
+  { // register read operation via I2C
     WIRE.beginTransmission(device_address_sspin);
     WIRE.write((reg_addr << 3 | channel << 1));
     WIRE.endTransmission(0);
     WIRE.requestFrom(device_address_sspin, (uint8_t)1);
     result = WIRE.read();
-  } else if (protocol == SC16IS750_PROTOCOL_SPI) { // register read operation via SPI
+  }
+  else if (protocol == SC16IS750_PROTOCOL_SPI)
+  { // register read operation via SPI
     ::digitalWrite(device_address_sspin, LOW);
     delayMicroseconds(10);
     SPI.transfer(0x80 | ((reg_addr << 3 | channel << 1)));
@@ -138,9 +148,9 @@ uint8_t SC16IS752::ReadRegister(uint8_t channel, uint8_t reg_addr)
     ::digitalWrite(device_address_sspin, HIGH);
   }
 
-#ifdef  SC16IS750_DEBUG_PRINT
+#ifdef SC16IS750_DEBUG_PRINT
   Serial.print("ReadRegister channel=");
-  Serial.print(channel,                        HEX);
+  Serial.print(channel, HEX);
   Serial.print(" reg_addr=");
   Serial.print((reg_addr << 3 | channel << 1), HEX);
   Serial.print(" result=");
@@ -151,21 +161,24 @@ uint8_t SC16IS752::ReadRegister(uint8_t channel, uint8_t reg_addr)
 
 void SC16IS752::WriteRegister(uint8_t channel, uint8_t reg_addr, uint8_t val)
 {
-#ifdef  SC16IS750_DEBUG_PRINT
+#ifdef SC16IS750_DEBUG_PRINT
   Serial.print("WriteRegister channel=");
-  Serial.print(channel,                        HEX);
+  Serial.print(channel, HEX);
   Serial.print(" reg_addr=");
   Serial.print((reg_addr << 3 | channel << 1), HEX);
   Serial.print(" val=");
   Serial.println(val, HEX);
 #endif // ifdef  SC16IS750_DEBUG_PRINT
 
-  if (protocol == SC16IS750_PROTOCOL_I2C) { // register read operation via I2C
+  if (protocol == SC16IS750_PROTOCOL_I2C)
+  { // register read operation via I2C
     WIRE.beginTransmission(device_address_sspin);
     WIRE.write((reg_addr << 3 | channel << 1));
     WIRE.write(val);
     WIRE.endTransmission(1);
-  } else {
+  }
+  else
+  {
     ::digitalWrite(device_address_sspin, LOW);
     delayMicroseconds(10);
     SPI.transfer((reg_addr << 3 | channel << 1));
@@ -177,9 +190,12 @@ void SC16IS752::WriteRegister(uint8_t channel, uint8_t reg_addr, uint8_t val)
 
 void SC16IS752::Initialize()
 {
-  if (protocol == SC16IS750_PROTOCOL_I2C) {
+  if (protocol == SC16IS750_PROTOCOL_I2C)
+  {
     WIRE.begin();
-  } else {
+  }
+  else
+  {
     ::pinMode(device_address_sspin, OUTPUT);
     ::digitalWrite(device_address_sspin, HIGH);
     SPI.setDataMode(SPI_MODE0);
@@ -196,35 +212,37 @@ void SC16IS752::Initialize()
 int16_t SC16IS752::SetBaudrate(uint8_t channel, uint32_t baudrate) // return error of baudrate parts per thousand
 {
   uint16_t divisor;
-  uint8_t  prescaler;
+  uint8_t prescaler;
   uint32_t actual_baudrate;
-  int16_t  error;
-  uint8_t  temp_lcr;
+  int16_t error;
+  uint8_t temp_lcr;
 
-  if ((ReadRegister(channel, SC16IS750_REG_MCR) & 0x80) == 0) { // if prescaler==1
+  if ((ReadRegister(channel, SC16IS750_REG_MCR) & 0x80) == 0)
+  { // if prescaler==1
     prescaler = 1;
-  } else {
+  }
+  else
+  {
     prescaler = 4;
   }
 
   divisor = (SC16IS750_CRYSTCAL_FREQ / prescaler) / (baudrate * 16);
 
-  temp_lcr  = ReadRegister(channel, SC16IS750_REG_LCR);
+  temp_lcr = ReadRegister(channel, SC16IS750_REG_LCR);
   temp_lcr |= 0x80;
   WriteRegister(channel, SC16IS750_REG_LCR, temp_lcr);
 
   // write to DLL
-  WriteRegister(channel, SC16IS750_REG_DLL,        (uint8_t)divisor);
+  WriteRegister(channel, SC16IS750_REG_DLL, (uint8_t)divisor);
 
   // write to DLH
-  WriteRegister(channel, SC16IS750_REG_DLH,        (uint8_t)(divisor >> 8));
+  WriteRegister(channel, SC16IS750_REG_DLH, (uint8_t)(divisor >> 8));
   temp_lcr &= 0x7F;
   WriteRegister(channel, SC16IS750_REG_LCR, temp_lcr);
 
-
   actual_baudrate = (SC16IS750_CRYSTCAL_FREQ / prescaler) / (16 * divisor);
-  error           = ((float)actual_baudrate - baudrate) * 1000 / baudrate;
-#ifdef  SC16IS750_DEBUG_PRINT
+  error = ((float)actual_baudrate - baudrate) * 1000 / baudrate;
+#ifdef SC16IS750_DEBUG_PRINT
   Serial.print("Desired baudrate: ");
   Serial.println(baudrate, DEC);
   Serial.print("Calculated divisor: ");
@@ -242,50 +260,53 @@ void SC16IS752::SetLine(uint8_t channel, uint8_t data_length, uint8_t parity_sel
 {
   uint8_t temp_lcr;
 
-  temp_lcr  = ReadRegister(channel, SC16IS750_REG_LCR);
+  temp_lcr = ReadRegister(channel, SC16IS750_REG_LCR);
   temp_lcr &= 0xC0; // Clear the lower six bit of LCR (LCR[0] to LCR[5]
-#ifdef  SC16IS750_DEBUG_PRINT
+#ifdef SC16IS750_DEBUG_PRINT
   Serial.print("LCR Register:0x");
   Serial.println(temp_lcr, DEC);
 #endif // ifdef  SC16IS750_DEBUG_PRINT
 
-  switch (data_length) { // data length settings
-    case 5:
-      break;
-    case 6:
-      temp_lcr |= 0x01;
-      break;
-    case 7:
-      temp_lcr |= 0x02;
-      break;
-    case 8:
-      temp_lcr |= 0x03;
-      break;
-    default:
-      temp_lcr |= 0x03;
-      break;
+  switch (data_length)
+  { // data length settings
+  case 5:
+    break;
+  case 6:
+    temp_lcr |= 0x01;
+    break;
+  case 7:
+    temp_lcr |= 0x02;
+    break;
+  case 8:
+    temp_lcr |= 0x03;
+    break;
+  default:
+    temp_lcr |= 0x03;
+    break;
   }
 
-  if (stop_length == 2) {
+  if (stop_length == 2)
+  {
     temp_lcr |= 0x04;
   }
 
-  switch (parity_select) { // parity selection length settings
-    case 0:                // no parity
-      break;
-    case 1:                // odd parity
-      temp_lcr |= 0x08;
-      break;
-    case 2:                // even parity
-      temp_lcr |= 0x18;
-      break;
-    case 3:                // force '1' parity
-      temp_lcr |= 0x03;
-      break;
-    case 4:                // force '0' parity
-      break;
-    default:
-      break;
+  switch (parity_select)
+  {       // parity selection length settings
+  case 0: // no parity
+    break;
+  case 1: // odd parity
+    temp_lcr |= 0x08;
+    break;
+  case 2: // even parity
+    temp_lcr |= 0x18;
+    break;
+  case 3: // force '1' parity
+    temp_lcr |= 0x03;
+    break;
+  case 4: // force '0' parity
+    break;
+  default:
+    break;
   }
 
   WriteRegister(channel, SC16IS750_REG_LCR, temp_lcr);
@@ -297,10 +318,13 @@ void SC16IS752::GPIOSetPinMode(uint8_t pin_number, uint8_t i_o)
 
   temp_iodir = ReadRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IODIR);
 
-  if (i_o == OUTPUT) {
+  if (i_o == OUTPUT)
+  {
     temp_iodir |= (0x01 << pin_number);
-  } else {
-    temp_iodir &= (uint8_t) ~(0x01 << pin_number);
+  }
+  else
+  {
+    temp_iodir &= (uint8_t)~(0x01 << pin_number);
   }
 
   WriteRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IODIR, temp_iodir);
@@ -312,10 +336,13 @@ void SC16IS752::GPIOSetPinState(uint8_t pin_number, uint8_t pin_state)
 
   temp_iostate = ReadRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IOSTATE);
 
-  if (pin_state == 1) {
+  if (pin_state == 1)
+  {
     temp_iostate |= (0x01 << pin_number);
-  } else {
-    temp_iostate &= (uint8_t) ~(0x01 << pin_number);
+  }
+  else
+  {
+    temp_iostate &= (uint8_t)~(0x01 << pin_number);
   }
 
   WriteRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IOSTATE, temp_iostate);
@@ -327,7 +354,8 @@ uint8_t SC16IS752::GPIOGetPinState(uint8_t pin_number)
 
   temp_iostate = ReadRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IOSTATE);
 
-  if ((temp_iostate & (0x01 << pin_number)) == 0) {
+  if ((temp_iostate & (0x01 << pin_number)) == 0)
+  {
     return 0;
   }
   return 1;
@@ -349,15 +377,18 @@ void SC16IS752::GPIOSetPortState(uint8_t port_state)
 }
 
 void SC16IS752::SetPinInterrupt(uint8_t pin_number, bool int_ena)
-{ 
+{
   uint8_t temp_iostate;
 
   temp_iostate = ReadRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IOINTENA);
 
-  if (int_ena == 1) {
+  if (int_ena == 1)
+  {
     temp_iostate |= (0x01 << pin_number);
-  } else {
-    temp_iostate &= (uint8_t) ~(0x01 << pin_number);
+  }
+  else
+  {
+    temp_iostate &= (uint8_t)~(0x01 << pin_number);
   }
 
   WriteRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IOINTENA, temp_iostate);
@@ -369,7 +400,8 @@ uint8_t SC16IS752::GetPinInterrupt(uint8_t pin_number)
 
   temp_iostate = ReadRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IOINTENA);
 
-  if ((temp_iostate & (0x01 << pin_number)) == 0) {
+  if ((temp_iostate & (0x01 << pin_number)) == 0)
+  {
     return 0;
   }
   return 1;
@@ -379,7 +411,7 @@ void SC16IS752::ResetDevice()
 {
   uint8_t reg;
 
-  reg  = ReadRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IOCONTROL);
+  reg = ReadRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IOCONTROL);
   reg |= 0x08;
   WriteRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IOCONTROL, reg);
 }
@@ -390,9 +422,12 @@ void SC16IS752::ModemPin(uint8_t gpio) // gpio == 0, gpio[7:4] are modem pins, g
 
   temp_iocontrol = ReadRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IOCONTROL);
 
-  if (gpio == 0) {
+  if (gpio == 0)
+  {
     temp_iocontrol |= 0x02;
-  } else {
+  }
+  else
+  {
     temp_iocontrol &= 0xFD;
   }
   WriteRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IOCONTROL, temp_iocontrol);
@@ -404,9 +439,12 @@ void SC16IS752::GPIOLatch(uint8_t latch)
 
   temp_iocontrol = ReadRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IOCONTROL);
 
-  if (latch == 0) {
+  if (latch == 0)
+  {
     temp_iocontrol &= 0xFE;
-  } else {
+  }
+  else
+  {
     temp_iocontrol |= 0x01;
   }
   WriteRegister(SC16IS752_CHANNEL_BOTH, SC16IS750_REG_IOCONTROL, temp_iocontrol);
@@ -426,29 +464,30 @@ void SC16IS752::__isr(uint8_t channel)
 {
   uint8_t irq_src;
 
-  irq_src  = ReadRegister(channel, SC16IS750_REG_IIR);
-  irq_src  = (irq_src >> 1);
+  irq_src = ReadRegister(channel, SC16IS750_REG_IIR);
+  irq_src = (irq_src >> 1);
   irq_src &= 0x3F;
 
-  switch (irq_src) {
-    case 0x06: // Receiver Line Status Error
-      break;
-    case 0x0c: // Receiver time-out interrupt
-      break;
-    case 0x04: // RHR interrupt
-      break;
-    case 0x02: // THR interrupt
-      break;
-    case 0x00: // modem interrupt;
-      break;
-    case 0x30: // input pin change of state
-      break;
-    case 0x10: // XOFF
-      break;
-    case 0x20: // CTS,RTS
-      break;
-    default:
-      break;
+  switch (irq_src)
+  {
+  case 0x06: // Receiver Line Status Error
+    break;
+  case 0x0c: // Receiver time-out interrupt
+    break;
+  case 0x04: // RHR interrupt
+    break;
+  case 0x02: // THR interrupt
+    break;
+  case 0x00: // modem interrupt;
+    break;
+  case 0x30: // input pin change of state
+    break;
+  case 0x10: // XOFF
+    break;
+  case 0x20: // CTS,RTS
+    break;
+  default:
+    break;
   }
 }
 
@@ -458,9 +497,12 @@ void SC16IS752::FIFOEnable(uint8_t channel, uint8_t fifo_enable)
 
   temp_fcr = ReadRegister(channel, SC16IS750_REG_FCR);
 
-  if (fifo_enable == 0) {
+  if (fifo_enable == 0)
+  {
     temp_fcr &= 0xFE;
-  } else {
+  }
+  else
+  {
     temp_fcr |= 0x01;
   }
   WriteRegister(channel, SC16IS750_REG_FCR, temp_fcr);
@@ -472,9 +514,12 @@ void SC16IS752::FIFOReset(uint8_t channel, uint8_t rx_fifo)
 
   temp_fcr = ReadRegister(channel, SC16IS750_REG_FCR);
 
-  if (rx_fifo == 0) {
+  if (rx_fifo == 0)
+  {
     temp_fcr |= 0x04;
-  } else {
+  }
+  else
+  {
     temp_fcr |= 0x02;
   }
   WriteRegister(channel, SC16IS750_REG_FCR, temp_fcr);
@@ -484,29 +529,33 @@ void SC16IS752::FIFOSetTriggerLevel(uint8_t channel, uint8_t rx_fifo, uint8_t le
 {
   uint8_t temp_reg;
 
-  temp_reg  = ReadRegister(channel, SC16IS750_REG_MCR);
+  temp_reg = ReadRegister(channel, SC16IS750_REG_MCR);
   temp_reg |= 0x04;
-  WriteRegister(channel, SC16IS750_REG_MCR, temp_reg);        // SET MCR[2] to '1' to use TLR register or trigger level control in FCR
-                                                              // register
+  WriteRegister(channel, SC16IS750_REG_MCR, temp_reg); // SET MCR[2] to '1' to use TLR register or trigger level control in FCR
+                                                       // register
 
   temp_reg = ReadRegister(channel, SC16IS750_REG_EFR);
   WriteRegister(channel, SC16IS750_REG_EFR, temp_reg | 0x10); // set ERF[4] to '1' to use the  enhanced features
 
-  if (rx_fifo == 0) {
-    WriteRegister(channel, SC16IS750_REG_TLR, length << 4);   // Tx FIFO trigger level setting
-  } else {
-    WriteRegister(channel, SC16IS750_REG_TLR, length);        // Rx FIFO Trigger level setting
+  if (rx_fifo == 0)
+  {
+    WriteRegister(channel, SC16IS750_REG_TLR, length << 4); // Tx FIFO trigger level setting
   }
-  WriteRegister(channel, SC16IS750_REG_EFR, temp_reg);        // restore EFR register
+  else
+  {
+    WriteRegister(channel, SC16IS750_REG_TLR, length); // Rx FIFO Trigger level setting
+  }
+  WriteRegister(channel, SC16IS750_REG_EFR, temp_reg); // restore EFR register
 }
 
 uint8_t SC16IS752::FIFOAvailableData(uint8_t channel)
 {
-#ifdef  SC16IS750_DEBUG_PRINT
+#ifdef SC16IS750_DEBUG_PRINT
   Serial.print("=====Available data:");
   Serial.println(ReadRegister(channel, SC16IS750_REG_RXLVL), DEC);
 #endif // ifdef  SC16IS750_DEBUG_PRINT
-  if (fifo_available[channel] == 0) {
+  if (fifo_available[channel] == 0)
+  {
     fifo_available[channel] = ReadRegister(channel, SC16IS750_REG_RXLVL);
   }
   return fifo_available[channel];
@@ -534,7 +583,8 @@ void SC16IS752::WriteByte(uint8_t channel, uint8_t val)
    #endif
      WriteRegister(SC16IS750_REG_THR,val);
    */
-  do {
+  do
+  {
     tmp_lsr = ReadRegister(channel, SC16IS750_REG_LSR);
   } while ((tmp_lsr & 0x20) == 0);
 
@@ -545,16 +595,20 @@ int SC16IS752::ReadByte(uint8_t channel)
 {
   volatile uint8_t val;
 
-  if (FIFOAvailableData(channel) == 0) {
-#ifdef  SC16IS750_DEBUG_PRINT
+  if (FIFOAvailableData(channel) == 0)
+  {
+#ifdef SC16IS750_DEBUG_PRINT
     Serial.println("No data available");
 #endif // ifdef  SC16IS750_DEBUG_PRINT
     return -1;
-  } else {
-#ifdef  SC16IS750_DEBUG_PRINT
+  }
+  else
+  {
+#ifdef SC16IS750_DEBUG_PRINT
     Serial.println("***********Data available***********");
 #endif // ifdef  SC16IS750_DEBUG_PRINT
-    if (fifo_available[channel] > 0) {
+    if (fifo_available[channel] > 0)
+    {
       --fifo_available[channel];
     }
     val = ReadRegister(channel, SC16IS750_REG_RHR);
@@ -568,9 +622,12 @@ void SC16IS752::EnableTransmit(uint8_t channel, uint8_t tx_enable)
 
   temp_efcr = ReadRegister(channel, SC16IS750_REG_EFCR);
 
-  if (tx_enable == 0) {
+  if (tx_enable == 0)
+  {
     temp_efcr |= 0x04;
-  } else {
+  }
+  else
+  {
     temp_efcr &= 0xFB;
   }
   WriteRegister(channel, SC16IS750_REG_EFCR, temp_efcr);
@@ -580,25 +637,29 @@ uint8_t SC16IS752::ping()
 {
   WriteRegister(SC16IS752_CHANNEL_A, SC16IS750_REG_SPR, 0x55);
 
-  if (ReadRegister(SC16IS752_CHANNEL_A, SC16IS750_REG_SPR) != 0x55) {
+  if (ReadRegister(SC16IS752_CHANNEL_A, SC16IS750_REG_SPR) != 0x55)
+  {
     return 0;
   }
 
   WriteRegister(SC16IS752_CHANNEL_A, SC16IS750_REG_SPR, 0xAA);
 
-  if (ReadRegister(SC16IS752_CHANNEL_A, SC16IS750_REG_SPR) != 0xAA) {
+  if (ReadRegister(SC16IS752_CHANNEL_A, SC16IS750_REG_SPR) != 0xAA)
+  {
     return 0;
   }
 
   WriteRegister(SC16IS752_CHANNEL_B, SC16IS750_REG_SPR, 0x55);
 
-  if (ReadRegister(SC16IS752_CHANNEL_B, SC16IS750_REG_SPR) != 0x55) {
+  if (ReadRegister(SC16IS752_CHANNEL_B, SC16IS750_REG_SPR) != 0x55)
+  {
     return 0;
   }
 
   WriteRegister(SC16IS752_CHANNEL_B, SC16IS750_REG_SPR, 0xAA);
 
-  if (ReadRegister(SC16IS752_CHANNEL_B, SC16IS750_REG_SPR) != 0xAA) {
+  if (ReadRegister(SC16IS752_CHANNEL_B, SC16IS750_REG_SPR) != 0xAA)
+  {
     return 0;
   }
 
@@ -611,34 +672,37 @@ uint8_t SC16IS752::ping()
         timeout = time_out;
    }
  */
- 
+
 size_t SC16IS752::readBytes(uint8_t channel, uint8_t *buffer, size_t length)
 {
-  size_t count=0;
+  size_t count = 0;
   int16_t tmp;
 
-  while (count < length) {
-	tmp = ReadByte(channel);
-	if (tmp < 0) {
-			break;
-	}
-	*buffer++ = tmp;
-	count++;
+  while (count < length)
+  {
+    tmp = ReadByte(channel);
+    if (tmp < 0)
+    {
+      break;
+    }
+    *buffer++ = tmp;
+    count++;
   }
   return count;
 }
-   
+
 String SC16IS752::readStringUntil(uint8_t channel, char terminator)
 {
   String ret;
   int c = ReadByte(channel);
-  while(c >= 0 && c != terminator) {
-	ret += (char) c;
-	c = ReadByte(channel);
+  while (c >= 0 && c != terminator)
+  {
+    ret += (char)c;
+    c = ReadByte(channel);
   }
   return ret;
 }
-   
+
 /*
    int16_t SC16IS752::readwithtimeout()
    {
@@ -656,17 +720,20 @@ void SC16IS752::flush(uint8_t channel)
 {
   uint8_t tmp_lsr;
 
-  do {
+  do
+  {
     tmp_lsr = ReadRegister(channel, SC16IS750_REG_LSR);
   } while ((tmp_lsr & 0x20) == 0);
 }
 
 int SC16IS752::peek(uint8_t channel)
 {
-  if (peek_flag[channel] == 0) {
+  if (peek_flag[channel] == 0)
+  {
     peek_buf[channel] = ReadByte(channel);
 
-    if (peek_buf[channel] >= 0) {
+    if (peek_buf[channel] >= 0)
+    {
       peek_flag[channel] = 1;
     }
   }
